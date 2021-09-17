7d914e2f63ba31dd448cd30305d3c5cfb33fdf4d
ALAM-97 <alexamara97@gmail.com>
2021-09-17 17:17:20 +0200

    Slider Added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
 const app = new Vue({
     el: '#root',
     data: {
-        
+        images: 
+        [
+            {
+                img: "img/slider-discord.png",
+                url: "https://alam-97-boolean-discord.netlify.app/"
+            },
+            {
+                img: "img/slider-dropbox.png",
+                url: "https://alam-97-boolean-dropbox.netlify.app/"
+            },
+            {
+                img: "img/slider-ps.png",
+                url: "https://alam-97-boolean-40-ps-store.netlify.app/"
+            },
+            {
+                img: "img/slider-spotify.png",
+                url: "https://alam-97-boolean-spotify.netlify.app/"
+            },
+            {
+                img: "img/slider-zoom.png",
+                url: "https://alam-97-boolean-zoom.netlify.app/"
+            }
+        ],
+        imageIndex: 0
     },
     methods: {
-
-    }
+        nextImage: function() {
+            this.imageIndex++
+            if (this.imageIndex >= this.images.length) {
+                this.imageIndex = 0;
+            }
+        },
+        previousImage: function() {
+            this.imageIndex--
+            if (this.imageIndex == -1) {
+                this.imageIndex = this.images.length - 1;
+            }
+        }
+    },
 })
